92f52a31ffb63cd933bfe3d2cc29a56c02856170
Drew Libberton <drew.libberton@gmail.com>
2024-01-12 12:48:50 -0500

    fixed size max sum problem example

--- slidingWindow.js
+++ slidingWindow.js
@@ -1,3 +1,35 @@
-/* this is a file for practice problems
+/* 1st problem
+fixed size sliding window
+comparing subarrays using windows and finding the new sum
+{1, 2, 3, 4, 5, 6}
+grab 123, compare to 234
+123 = 6, 234 = 9,
+subarray 234 isnt added together directly
+it removes arr[0] and adds arr[3] frem the already added subarray
 */ 
-let i = 0
+function maxSubarraySum(arr, num){
+    if (num > arr.length){
+        return null;
+    }
+    // num refers to window size, if windowsize has more elements than the array it will null out
+let maxSum = 0;
+let tempSum = 0;
+// initialize variable for final solution and keeping track of current total
+for (let i = 0; i < num; i++){
+    maxSum += arr[i];
+}
+// initialize the window
+tempSum = maxSum;
+// set sums = to each other
+for (let i = num; i < arr.length; i++){
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+}
+// slide the window over the array
+return maxSum;
+}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const num = 3;
+const maxSum = maxSubarraySum(arr, num)
+console.log(maxSum) // 24, maxSum of 3 num array (7, 8, 9)
+//-----------------------------------------------------------
